fa5637872219433133d8f91b79b439bebae8004c
Nybble33 <cadodbd1@gmail.com>
2013-12-18 08:59:32 +0000

    Work, some problems with spaces

--- packcss.py
+++ packcss.py
@@ -4,46 +4,45 @@
 import re
 from subprocess import Popen
 
+
 def oneString(_string_):
     pattern = re.compile('(?s)\n\s*')
     __foo = pattern.sub('', _string_)
-    pattern = re.compile('\s*')
+    pattern = re.compile('\{\s*\}')
     return pattern.sub('', __foo)
+    
 
-def compressCSS(filename):
-    code = filename.read()
+def compressCSS(code):
     p = re.compile('(?s)/\*.*?\*/')
     code = p.sub('', code)
     p = re.compile('(?s)[^}](.*?\})')
     ms = p.findall(code)
+    foo = ''
     for m in ms:
-        print '-------------------'
-        print m
-        print oneString(m)
-    print '--- end parsing'
+        foo += oneString(m) + '\n'
+    return foo
 
 
 FILES = [
     {
         'directory': '/home/admin/radomir.biz/mysite/static/css_new/',
-        'outputFile': 'nybble_style.min.css',
+        'outputFile': 'radomir_style.min.css',
         'files': [
-            'nybble_style.css'
+            'nybble_style.css',
+            'style.css'
         ]
     }
 ]
 
 if __name__ == '__main__':
     print 'Begin combining files...'
+    codeToCompress = ''
     for FILE in FILES:
-        fullPath = FILE['directory'] + FILE['outputFile']+'.tmp'
+        for cssFile in FILE['files']:
+            _fullPath = FILE['directory'] + cssFile
+            print 'Reading file - ' + _fullPath
+            codeToCompress += open(_fullPath, 'r').read()
+        fullPath = FILE['directory'] + FILE['outputFile']
         print 'Creating ouput file - ' + fullPath
         outputFile = open(fullPath, 'w')
-        for jsFile in FILE['files']:
-            _fullPath = FILE['directory'] + jsFile
-            print 'Reading file - ' + _fullPath
-            code = open(_fullPath, 'r').read()
-            outputFile.write(code)
-        outputFile.close()
-        outputFile = open(fullPath, 'r')
-        compressCSS(outputFile)
+        outputFile.write(compressCSS(codeToCompress))
